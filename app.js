20d1ae8abae9f3db1e5487200a0f3aed83bdd008
elsaaeid <saidsadaoy@gmail.com>
2023-06-12 05:09:01 -0700

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 require("./db/conn");
 const router = require("./Routes/router");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 
 
